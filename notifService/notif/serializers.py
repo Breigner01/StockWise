b48632c1b3368b4720160313d6bf2215afa9cb88
Peter Delis <delispeter19@gmail.com>
2023-03-30 14:53:05 -0400

    Add urls, views, serializers and models for notifs

---
+++ notifService/notif/serializers.py
@@ -0,0 +1,25 @@
+from rest_framework import serializers
+from .models import *
+
+class NotifSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notif
+        fields = "__all__"
+
+class ComplexNotifSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Notif
+        fields = "__all__"
+        extra_kwargs = {
+            'type':{'read_only':True}
+            }
+        
+    def create(self, validated_data):
+        # logic on notif
+
+        complexNotif = Notif.objects.create(
+            type = "Default",
+            **validated_data
+        )
+
+        return complexNotif
--- notifService/notif/views.py
+++ notifService/notif/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from rest_framework import viewsets, generics
+from rest_framework.response import Response
+from .serializers import *
 
-# Create your views here.
+# Generic view interaction (basically CRUD)
+class NotifView(viewsets.ModelViewSet):
+    serializer_class = NotifSerializer
+    
+    def get_queryset(self):
+        return Notif.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+# This is just an example of how we can perform complex logic of different types of requests
+class ComplexNotifView(generics.GenericAPIView):
+
+    # Making my own custom get
+    def get(self, request, *args, **kwargs):
+        notifs = []
+
+        notif_query_set = Notif.objects.all()
+        for notif_model in notif_query_set:
+            notif = ComplexNotifSerializer(notif_model).data
+            notif['subject'] = "Complex Modification"
+            notifs.append(notif)
+
+        return Response(notifs)
