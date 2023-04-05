dc8d0418cf2950b0c0988a7609c557cb20de5116
Gabriel Martinica <gabmartinica@gmail.com>
2023-04-05 01:25:59 -0400

    Add firebase admin token verification

--- auth/app.js
+++ auth/app.js
@@ -2,8 +2,8 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const { admin } = require("./firebaseConfig");
-
+const { admin, auth } = require("./firebaseConfig");
+const { signInWithEmailAndPassword } = require("firebase/auth");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -28,12 +28,81 @@ app.post("/register", (req, res) => {
         })
         .then((userRecord) => {
             console.log("User created:", userRecord.uid);
+            userToken = userRecord.uid;
             res.redirect("/");
         })
         .catch((error) => {
             console.log("Error creating user:", error);
             res.redirect("register");
         });
+});
+
+app.get("/login", (req, res) => {
+    res.render("login");
+});
+
+// app.get("/test", (req, res) => {
+//     signInWithEmailAndPassword(auth, "test@example.com", "testPassword")
+//         .then((userCredential) => {
+//             // Signed in successfully
+//             const user = userCredential.user;
+//             console.log("User signed in:", user.uid);
+//         })
+//         .catch((error) => {
+//             // Handle errors here
+//             console.error("Error signing in:", error);
+//         });
+// });
+
+app.post("/login", (req, res) => {
+    const { email, password } = req.body;
+
+    signInWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // User logged in successfully
+            const user = userCredential.user;
+            return auth.currentUser.getIdToken(/* forceRefresh */ true);
+        })
+        .then((idToken) => {
+            // Send the token to the backend
+            fetch("http://localhost:3000/secureToken", {
+                method: "GET",
+                headers: {
+                    Authorization: "Bearer " + idToken,
+                },
+            });
+        })
+        .catch((error) => {
+            // Handle login error
+            console.log("Error signing in:" + error);
+        })
+        .finally(() => {
+            res.render("index");
+        });
+});
+
+const authMiddleware = (req, res, next) => {
+    const token =
+        req.headers.authorization && req.headers.authorization.split(" ")[1];
+    if (!token) return res.status(401).send("Unauthorized");
+    admin
+        .auth()
+        .verifyIdToken(token)
+        .then((decodedToken) => {
+            req.user = decodedToken;
+            next();
+        })
+        .catch((error) => {
+            res.status(401).send("Unauthorized");
+        });
+};
+
+module.exports = authMiddleware;
+
+app.get("/secureToken", authMiddleware, (req, res) => {
+    // The user's decoded token is available in req.user
+    console.log("Route handler called");
+    console.log(req.user);
 });
 
 const PORT = process.env.PORT || 3000;
--- auth/firebaseConfig.js
+++ auth/firebaseConfig.js
@@ -1,14 +1,42 @@
-
-
 // firebaseConfig.js
 
 const admin = require("firebase-admin");
+const { initializeApp } = require("firebase/app");
+const { getAuth, signInWithEmailAndPassword } = require("firebase/auth");
 const serviceAccount = require("/Users/gabriel/Downloads/warehouse-app-abe08-firebase-adminsdk-nbtit-81f28ab8b2.json");
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount),
 });
 
-module.exports = {
-  admin,
+// Load environment variables
+require("dotenv").config();
+console.log(process.env.FIREBASE_API_KEY);
+const firebaseConfig = {
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID,
 };
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Firebase Authentication and get a reference to the service
+const auth = getAuth(app);
+
+// // Use the auth instance for authentication operations
+// signInWithEmailAndPassword(auth, "a@a.com", "concordia")
+//     .then((userCredential) => {
+//         // Signed in
+//         var user = userCredential.user;
+//         console.log(user);
+//         // ...
+//     })
+//     .catch((error) => {
+//         var errorCode = error.code;
+//         var errorMessage = error.message;
+//     });
+
+module.exports = { auth, admin /* your admin instance */ };
