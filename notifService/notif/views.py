e1a3d00d39365c9af1396e5626b12a53786ba45f
delispeter19 <48844162+delispeter19@users.noreply.github.com>
2023-04-05 13:43:41 -0400

    Update Notif app models (#10)
    * Add owner model and link to notification model
    
    * Add owner views, serializers and urls

--- notifService/notif/views.py
+++ notifService/notif/views.py
@@ -1,19 +1,68 @@
-from rest_framework import viewsets, generics
+from rest_framework import viewsets, generics, permissions
 from rest_framework.response import Response
 from .serializers import *
 
 # Generic view interaction (basically CRUD)
+class OwnerView(viewsets.ModelViewSet):
+    
+    permission_classes = [permissions.IsAdminUser]
+
+    serializer_class = OwnerSerializer
+    
+    def get_queryset(self):
+        return Owner.objects.all()
+
+    def perform_create(self, serializer):
+        serializer.save()
+
+# Generic view interaction (basically CRUD)
 class NotifView(viewsets.ModelViewSet):
+
+    permission_classes = [permissions.IsAdminUser]
+
     serializer_class = NotifSerializer
     
     def get_queryset(self):
         return Notif.objects.all()
 
     def perform_create(self, serializer):
-        serializer.save()
+        if ("owner" in self.request.data):
+            serializer.save(owner=Owner.objects.get(id=self.request.data["owner"]))
+        else:
+            serializer.save()
 
 # This is just an example of how we can perform complex logic of different types of requests
 class ComplexNotifView(generics.GenericAPIView):
+
+    # Making my own custom get
+    def get(self, request, *args, **kwargs):
+        notifs = []
+
+        notif_query_set = Notif.objects.all()
+        for notif_model in notif_query_set:
+            notif = ComplexNotifSerializer(notif_model).data
+            notif['subject'] = "Complex Modification"
+            notifs.append(notif)
+
+        return Response(notifs)
+    
+# This is just an example of how we can perform complex logic of different types of requests
+class NotifyOwnerView(generics.GenericAPIView):
+
+    # Making my own custom get
+    def get(self, request, *args, **kwargs):
+        notifs = []
+
+        notif_query_set = Notif.objects.all()
+        for notif_model in notif_query_set:
+            notif = ComplexNotifSerializer(notif_model).data
+            notif['subject'] = "Complex Modification"
+            notifs.append(notif)
+
+        return Response(notifs)
+    
+# This is just an example of how we can perform complex logic of different types of requests
+class NotifyAllView(generics.GenericAPIView):
 
     # Making my own custom get
     def get(self, request, *args, **kwargs):
