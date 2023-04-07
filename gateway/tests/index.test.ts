5a242d471dba66483ff943ec7514e8b8b816d8f5
Gabriel Martinica <gabmartinica@gmail.com>
2023-04-07 18:52:55 -0400

    Change test to cleaner promise syntax

--- gateway/tests/index.test.ts
+++ gateway/tests/index.test.ts
@@ -3,18 +3,22 @@ import app from '../src/index';
 
 describe('GraphQL API Gateway', () => {
   // Test cases for the authMiddleware
-  it('should return 401 if no token is provided', async () => {
-    const response = await request(app).post('/graphql').send({ query: '{ exampleQuery }' });
-    expect(response.status).toBe(401);
+  it('should return 401 if no token is provided', () => {
+    return request(app)
+      .post('/graphql')
+      .send({ query: '{ exampleQuery }' })
+      .then((response) => {
+        expect(response.status).toBe(401);
+      });
   });
 
-  it('should return 401 if an invalid token is provided', async () => {
-    const response = await request(app)
+  it('should return 401 if an invalid token is provided', () => {
+    return request(app)
       .post('/graphql')
       .set('Authorization', 'Bearer invalid_token')
-      .send({ query: '{ exampleQuery }' });
-    expect(response.status).toBe(401);
+      .send({ query: '{ exampleQuery }' })
+      .then((response) => {
+        expect(response.status).toBe(401);
+      });
   });
-
-
 });
