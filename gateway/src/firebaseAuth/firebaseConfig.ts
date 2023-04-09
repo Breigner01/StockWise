57e7812f49f99ac6a1bedbe579392072dc512722
Gabriel Martinica <gabmartinica@gmail.com>
2023-04-09 19:29:49 -0400

    Add auth gateway test to github actions (#18)
    * Add auth gateway test to github actions
    
    * Convert firebase to base64 for github actions

--- gateway/src/firebaseAuth/firebaseConfig.ts
+++ gateway/src/firebaseAuth/firebaseConfig.ts
@@ -1,5 +1,10 @@
 import * as admin from "firebase-admin";
-const serviceAccount = require("./firebaseKeys.json");
+require('dotenv').config();
+
+// Decode env base64 key
+const firebaseKey64 = process.env.FIREBASE_AUTH;
+const firebaseKey = Buffer.from(firebaseKey64, 'base64').toString('utf-8');
+const serviceAccount = JSON.parse(firebaseKey);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
