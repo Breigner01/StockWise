586518d1e66fbbd0bc6935582af9546df9a62438
Benjamin Reigner <benjamin.reigner@epitech.eu>
2023-04-11 19:05:21 -0400

    feat(product): add connection the DB

--- productService/config/structs.go
+++ productService/config/structs.go
@@ -1,13 +1,25 @@
 package config
 
 import (
+	"github.com/Breigner01/SOEN487-Project3/productService/ent"
 	"github.com/Breigner01/SOEN487-Project3/productService/server"
 )
 
-type Config struct {
-	Sv *server.Server
+type PostgresConfig struct {
+	Username string `mapstructure:"username"`
+	Password string `mapstructure:"password"`
+	Host     string `mapstructure:"host"`
+	Database string `mapstructure:"database"`
+	Port     string `mapstructure:"port"`
+	SSLMode  string `mapstructure:"sslmode"`
 }
 
 type ProgramConfig struct {
-	Port string
+	Port     string         `mapstructure:"port"`
+	Postgres PostgresConfig `mapstructure:"postgres"`
+}
+
+type Config struct {
+	Sv *server.Server
+	DB *ent.Client
 }
