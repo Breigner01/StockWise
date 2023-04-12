c65de430d28e5a0d756aabd157a5cd8b666269c1
Peter Delis <delispeter19@gmail.com>
2023-04-11 20:27:00 -0400

    Add google auth to login

--- frontend/src/redux/actions/authActions.js
+++ frontend/src/redux/actions/authActions.js
@@ -1,4 +1,6 @@
 import { 
+    signInWithPopup,
+    GoogleAuthProvider,
     getAuth,
     signInWithEmailAndPassword,
     createUserWithEmailAndPassword,
@@ -8,6 +10,34 @@ import {
 
 import { USER_LOADED, USER_LOADING, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL } from './types';
 import { fbApp } from "../../Firebase";
+
+export const googleLogin = () => () => {
+    const auth = getAuth();
+
+    const provider = new GoogleAuthProvider();
+    
+    signInWithPopup(auth, provider)
+        .then((result) => {
+            console.log(result);
+            // This gives you a Google Access Token. You can use it to access the Google API.
+            const credential = GoogleAuthProvider.credentialFromResult(result);
+            const token = credential.accessToken;
+            // The signed-in user info.
+            const user = result.user;
+            // IdP data available using getAdditionalUserInfo(result)
+            // ...
+        }).catch((error) => {
+            console.log(error);
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.customData.email;
+            // The AuthCredential type that was used.
+            const credential = GoogleAuthProvider.credentialFromError(error);
+            // ...
+        });
+}
 
 // LOAD USER
 export const loadUser = () => (dispatch) => {
