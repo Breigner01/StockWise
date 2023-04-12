c71461f173406cbcb42b6a4b9ca5fbb7c964453c
Benjamin Reigner <benjamin.reigner@epitech.eu>
2023-04-12 00:45:54 -0400

    feat(product): add product service functions

---
+++ productService/services/product/create.go
@@ -0,0 +1,44 @@
+package product
+
+import (
+	"fmt"
+	"github.com/Breigner01/SOEN487-Project3/productService/config"
+	brandDB "github.com/Breigner01/SOEN487-Project3/productService/database/brand"
+	categoryDB "github.com/Breigner01/SOEN487-Project3/productService/database/category"
+	productDB "github.com/Breigner01/SOEN487-Project3/productService/database/product"
+	"github.com/Breigner01/SOEN487-Project3/productService/ent"
+	product "github.com/Breigner01/SOEN487-Project3/productService/product"
+)
+
+func CreateProduct(conf config.Config, p *product.Product) error {
+
+	brand, err := brandDB.GetByName(conf.DB, p.Brand)
+	if err != nil {
+		return err
+	} else if brand == nil {
+		return fmt.Errorf("brand %s does not exist", p.Brand)
+	}
+
+	category, err := categoryDB.GetByName(conf.DB, p.Category)
+	if err != nil {
+		return err
+	} else if category == nil {
+		return fmt.Errorf("category %s does not exist", p.Category)
+	}
+
+	_, err = productDB.CreateProduct(conf.DB, &ent.Product{
+		ID:          0,
+		Name:        p.Name,
+		Description: p.Description,
+		Price:       p.Price,
+		Edges: ent.ProductEdges{
+			Brand:    brand,
+			Category: category,
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ productService/services/product/search.go
@@ -0,0 +1,73 @@
+package product
+
+import (
+	"github.com/Breigner01/SOEN487-Project3/productService/config"
+	productDB "github.com/Breigner01/SOEN487-Project3/productService/database/product"
+	product "github.com/Breigner01/SOEN487-Project3/productService/product"
+)
+
+func SearchProductsByName(conf config.Config, name string) ([]*product.Product, error) {
+
+	products, err := productDB.SearchProductsByName(conf.DB, name)
+	if err != nil {
+		return nil, err
+	}
+
+	var productsList []*product.Product
+	for _, p := range products {
+		productsList = append(productsList, &product.Product{
+			Id:          int64(p.ID),
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Brand:       p.Edges.Brand.Name,
+			Category:    p.Edges.Category.Name,
+		})
+	}
+
+	return productsList, nil
+}
+
+func SearchProductsByBrand(conf config.Config, brandName string) ([]*product.Product, error) {
+
+	products, err := productDB.SearchProductsByBrand(conf.DB, brandName)
+	if err != nil {
+		return nil, err
+	}
+
+	var productsList []*product.Product
+	for _, p := range products {
+		productsList = append(productsList, &product.Product{
+			Id:          int64(p.ID),
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Brand:       p.Edges.Brand.Name,
+			Category:    p.Edges.Category.Name,
+		})
+	}
+
+	return productsList, nil
+}
+
+func SearchProductsByCategory(conf config.Config, categoryName string) ([]*product.Product, error) {
+
+	products, err := productDB.SearchProductsByCategory(conf.DB, categoryName)
+	if err != nil {
+		return nil, err
+	}
+
+	var productsList []*product.Product
+	for _, p := range products {
+		productsList = append(productsList, &product.Product{
+			Id:          int64(p.ID),
+			Name:        p.Name,
+			Description: p.Description,
+			Price:       p.Price,
+			Brand:       p.Edges.Brand.Name,
+			Category:    p.Edges.Category.Name,
+		})
+	}
+
+	return productsList, nil
+}
