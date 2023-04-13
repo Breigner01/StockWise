7feddc59c70427f6f4f906175e3a05ec8b3109d4
Peter Delis <delispeter19@gmail.com>
2023-04-13 12:34:08 -0400

    Add get and create product queries and update auth actions

--- frontend/src/redux/reducers/authReducer.js
+++ frontend/src/redux/reducers/authReducer.js
@@ -1,7 +1,7 @@
 import { USER_LOADING, USER_LOADED, AUTH_ERROR, LOGIN_SUCCESS, LOGIN_FAIL, LOGOUT_SUCCESS, REGISTER_SUCCESS, REGISTER_FAIL } from "../actions/types";
 
 const initialState = {
-    token: localStorage.getItem('token'),
+    token: null,
     isAuthenticated: null,
     isLoading: false,
     user: null
@@ -17,14 +17,7 @@ export default function(state=initialState, action){
                 isLoading: true
             };
         case USER_LOADED:
-            return {
-                ...state,
-                isAuthenticated: true,
-                isLoading: false,
-                user: action.payload.user
-            };
         case LOGIN_SUCCESS:
-            localStorage.setItem("token", action.payload.token);
             return {
                 ...state,
                 token: action.payload.token,
@@ -36,7 +29,6 @@ export default function(state=initialState, action){
         case LOGIN_FAIL:
         case LOGOUT_SUCCESS:
         case REGISTER_FAIL:
-            localStorage.removeItem("token");
             return {
                 ...state,
                 token: null,
