e87122515b7b1fe74d74cc4b4bce243af091725c
Benjamin Reigner <benjamin.reigner@epitech.eu>
2023-04-16 23:55:02 -0400

    feat(product): add brand creation upon creating & updating a product (#49)

--- productService/methods_services/product/update.go
+++ productService/methods_services/product/update.go
@@ -1,6 +1,7 @@
 package product
 
 import (
+	"fmt"
 	"github.com/Breigner01/SOEN487-Project3/productService/config"
 	"github.com/Breigner01/SOEN487-Project3/productService/ent"
 	brandDB "github.com/Breigner01/SOEN487-Project3/productService/methods_database/brand"
@@ -14,11 +15,17 @@ func UpdateProduct(conf config.Config, p *product.Product) error {
 	brand, err := brandDB.GetByName(conf.DB, p.Brand)
 	if err != nil {
 		return err
+	} else if brand == nil {
+		brand, err = brandDB.CreateBrand(conf.DB, &ent.Brand{
+			Name: p.Brand,
+		})
 	}
 
 	category, err := categoryDB.GetByName(conf.DB, p.Category)
 	if err != nil {
 		return err
+	} else if category == nil {
+		return fmt.Errorf("category %s does not exist", p.Category)
 	}
 
 	_, err = productDB.UpdateProduct(conf.DB, &ent.Product{
