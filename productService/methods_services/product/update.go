f9457f138aa14747aa44eb09ac129e518e60c8cc
Benjamin Reigner <benjamin.reigner@epitech.eu>
2023-04-16 21:20:17 -0400

    fix(product): update method not working (#48)

--- productService/methods_services/product/update.go
+++ productService/methods_services/product/update.go
@@ -3,23 +3,32 @@ package product
 import (
 	"github.com/Breigner01/SOEN487-Project3/productService/config"
 	"github.com/Breigner01/SOEN487-Project3/productService/ent"
+	brandDB "github.com/Breigner01/SOEN487-Project3/productService/methods_database/brand"
+	categoryDB "github.com/Breigner01/SOEN487-Project3/productService/methods_database/category"
 	productDB "github.com/Breigner01/SOEN487-Project3/productService/methods_database/product"
 	product "github.com/Breigner01/SOEN487-Project3/productService/pb/product"
 )
 
 func UpdateProduct(conf config.Config, p *product.Product) error {
 
-	_, err := productDB.UpdateProduct(conf.DB, &ent.Product{
-		ID:    int(p.Id),
-		Name:  p.Name,
-		Price: p.Price,
+	brand, err := brandDB.GetByName(conf.DB, p.Brand)
+	if err != nil {
+		return err
+	}
+
+	category, err := categoryDB.GetByName(conf.DB, p.Category)
+	if err != nil {
+		return err
+	}
+
+	_, err = productDB.UpdateProduct(conf.DB, &ent.Product{
+		ID:          int(p.Id),
+		Name:        p.Name,
+		Price:       p.Price,
+		Description: p.Description,
 		Edges: ent.ProductEdges{
-			Brand: &ent.Brand{
-				Name: p.Brand,
-			},
-			Category: &ent.Category{
-				Name: p.Category,
-			},
+			Brand:    brand,
+			Category: category,
 		},
 	})
 
