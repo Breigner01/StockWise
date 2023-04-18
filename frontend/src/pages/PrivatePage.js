c8c79a5694ee5e2ac4a3b9559200232f5400c146
p-carrier <p-carrier@users.noreply.github.com>
2023-04-18 11:31:16 -0400

    update graphQl message to match schema

--- frontend/src/pages/PrivatePage.js
+++ frontend/src/pages/PrivatePage.js
@@ -3,155 +3,128 @@ import "../App.css";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import { getProducts, addProduct, deleteProduct } from "../redux/actions/productActions";
-import { getInventory, addInventory, decreaseInventory } from "../redux/actions/inventoryActions";
-
 import {
-    Button,
-    Box,
-    Typography,
-    Grid
-} from "@mui/material";
+  getProducts,
+  addProduct,
+  deleteProduct,
+} from "../redux/actions/productActions";
+import {
+  getInventory,
+  addInventory,
+  decreaseInventory,
+} from "../redux/actions/inventoryActions";
+
+import { Button, Box, Typography, Grid } from "@mui/material";
 
 const PrivatePage = (props) => {
+  const handleGetProducts = (e) => {
+    e.preventDefault();
+    props.getProducts(props.auth.user.uid);
+  };
 
-    const handleGetProducts = (e) => {
-        e.preventDefault();
-        props.getProducts(props.auth.user.uid);
+  const handleAddProduct = (e) => {
+    e.preventDefault();
+    const productData = {
+      id: 5,
+      name: "name",
+      brand: "gucci",
+      description: "sick product trust",
+      price: 20.99,
+      category: "A",
     };
+    props.addProduct("abc123", productData);
+  };
 
-    const handleAddProduct = (e) => {
-        e.preventDefault();
-        const productData = {
-            id: 5,
-            name: "name",
-            brand: "gucci",
-            description: "sick product trust",
-            price: 20.99,
-            category: "A",
-          }
-        props.addProduct("abc123", productData);
+  const handleDeleteProduct = (e) => {
+    e.preventDefault();
+    props.deleteProduct("abc123", 23);
+  };
+
+  const handleGetInventory = (e) => {
+    e.preventDefault();
+    props.getInventory("firebase_id", "sku");
+    console.log("object");
+  };
+
+  const handleAddInventory = (e) => {
+    e.preventDefault();
+    const invData = {
+      ownerId: "firebase_id",
+      sku: 1,
+      quantity: 1,
     };
+    props.addInventory("firebase_id", invData);
+  };
 
-    const handleDeleteProduct = (e) => {
-        e.preventDefault();
-        props.deleteProduct("abc123", 23)
+  const handleDecreaseInventory = (e) => {
+    e.preventDefault();
+    const invData = {
+      ownerId: "firebase_id",
+      sku: 1,
+      quantity: 1,
     };
+    props.decreaseInventory("firebase_id", invData);
+  };
 
-    const handleGetInventory = (e) => {
-        e.preventDefault();
-        props.getInventory("QQP4wAWbWaeO2sDdSfvU0Ac3NTg2", "111");
-    };
-
-    const handleAddInventory = (e) => {
-        e.preventDefault();
-        const invData = {
-            id: 5,
-            name: "name",
-            brand: "gucci",
-            description: "sick product trust",
-            price: 20.99,
-            category: "A",
-          }
-        props.addInventory("abc123", invData);
-    };
-
-    const handleDecreaseInventory = (e) => {
-        e.preventDefault();
-        const invData = {
-            id: 5,
-            name: "name",
-            brand: "gucci",
-            description: "sick product trust",
-            price: 20.99,
-            category: "A",
-          }
-        props.decreaseInventory("abc123", invData);
-    };
-
-    return (
-        <Grid container spacing={1} >
-            <Grid item xs={12}>
-                <header className="App-header">
-                    <Typography> Private Access </Typography>
-                    <Typography variant="h5">
-                        Welcome {props.auth.user.email}!
-                    </Typography>
-                    <Box component="form" onSubmit={handleGetProducts} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Get Products
-                        </Button>
-                    </Box>
-                    <Box component="form" onSubmit={handleAddProduct} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Add Products
-                        </Button>
-                    </Box>
-                    <Box component="form" onSubmit={handleDeleteProduct} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Delete Product
-                        </Button>
-                    </Box>
-                    <Box component="form" onSubmit={handleGetInventory} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Get Inventory
-                        </Button>
-                    </Box>
-                    <Box component="form" onSubmit={handleAddInventory} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Add Inventory
-                        </Button>
-                    </Box>
-                    <Box component="form" onSubmit={handleDecreaseInventory} sx={{ mt: 1 }}>
-                        <Button
-                            type="submit"
-                            fullWidth
-                            variant="contained"
-                            color="success"
-                        >
-                            Decrease Inventory
-                        </Button>
-                    </Box>
-                </header>
-            </Grid>
-        </Grid>
-    );
-}
+  return (
+    <Grid container spacing={1}>
+      <Grid item xs={12}>
+        <header className="App-header">
+          <Typography> Private Access </Typography>
+          <Typography variant="h5">Welcome {props.auth.user.email}!</Typography>
+          <Box component="form" onSubmit={handleGetProducts} sx={{ mt: 1 }}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Get Products
+            </Button>
+          </Box>
+          <Box component="form" onSubmit={handleAddProduct} sx={{ mt: 1 }}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Add Products
+            </Button>
+          </Box>
+          <Box component="form" onSubmit={handleDeleteProduct} sx={{ mt: 1 }}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Delete Product
+            </Button>
+          </Box>
+          <Box component="form" onSubmit={handleGetInventory} sx={{ mt: 1 }}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Get Inventory
+            </Button>
+          </Box>
+          <Box component="form" onSubmit={handleAddInventory} sx={{ mt: 1 }}>
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Add Inventory
+            </Button>
+          </Box>
+          <Box
+            component="form"
+            onSubmit={handleDecreaseInventory}
+            sx={{ mt: 1 }}
+          >
+            <Button type="submit" fullWidth variant="contained" color="success">
+              Decrease Inventory
+            </Button>
+          </Box>
+        </header>
+      </Grid>
+    </Grid>
+  );
+};
 
 PrivatePage.propTypes = {
-    auth: PropTypes.object.isRequired
+  auth: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
-    auth: state.authReducer
+  auth: state.authReducer,
 });
 
 export default connect(mapStateToProps, {
-    getProducts, addProduct, deleteProduct,
-    getInventory, addInventory, decreaseInventory
+  getProducts,
+  addProduct,
+  deleteProduct,
+  getInventory,
+  addInventory,
+  decreaseInventory,
 })(PrivatePage);
